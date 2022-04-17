c8382d00670355b0d1cbf0b9bc2cc58ae188e55f
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-04-17 05:05:27 +0300

    divide Appmodule to LocalModule and RemoteModule

--- app/src/main/java/com/suveybesena/shoppingapp/data/di/LocalModule.kt
+++ app/src/main/java/com/suveybesena/shoppingapp/data/di/LocalModule.kt
@@ -1,47 +1,19 @@
-package com.suveybesena.shoppingapp.di
+package com.suveybesena.shoppingapp.data.di
 
 import android.content.Context
 import androidx.room.Room
-import com.suveybesena.shoppingapp.common.Constants
 import com.suveybesena.shoppingapp.data.local.ProductDAO
 import com.suveybesena.shoppingapp.data.local.ProductsDatabase
-import com.suveybesena.shoppingapp.data.remote.MakeupItemsAPI
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AppModule {
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(
-        gsonConverterFactory: GsonConverterFactory
-    ): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(Constants.API_URL)
-            .addConverterFactory(gsonConverterFactory)
-            .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideGsonConverterFactory(): GsonConverterFactory {
-        return GsonConverterFactory.create()
-    }
-
-    @Provides
-    @Singleton
-    fun provideMyAPI(retrofit: Retrofit): MakeupItemsAPI {
-        return retrofit.create(MakeupItemsAPI::class.java)
-    }
-
+object LocalModule {
 
     @Provides
     @Singleton
@@ -56,5 +28,4 @@ object AppModule {
     fun provideDao(productsDatabase: ProductsDatabase): ProductDAO {
         return productsDatabase.productDao()
     }
-
 }
