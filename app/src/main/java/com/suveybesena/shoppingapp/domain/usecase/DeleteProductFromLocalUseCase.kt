62417197903b4771f891c70bc4e913747a083427
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-05-08 00:30:10 +0300

    add Dispatchers.IO

--- app/src/main/java/com/suveybesena/shoppingapp/domain/usecase/DeleteProductFromLocalUseCase.kt
+++ app/src/main/java/com/suveybesena/shoppingapp/domain/usecase/DeleteProductFromLocalUseCase.kt
@@ -3,7 +3,9 @@ package com.suveybesena.shoppingapp.domain.usecase
 import com.suveybesena.shoppingapp.common.Resource
 import com.suveybesena.shoppingapp.data.model.ProductFeatures
 import com.suveybesena.shoppingapp.data.repository.ShoppingRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class DeleteProductFromLocalUseCase @Inject constructor(val repository: ShoppingRepository) {
@@ -16,5 +18,5 @@ class DeleteProductFromLocalUseCase @Inject constructor(val repository: Shopping
         } catch (e: Exception) {
             emit(Resource.Error(e.localizedMessage))
         }
-    }
+    }.flowOn(Dispatchers.IO)
 }
