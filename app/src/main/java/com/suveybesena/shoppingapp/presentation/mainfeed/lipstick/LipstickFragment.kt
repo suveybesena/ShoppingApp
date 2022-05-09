b0986b5586b11b5f936f8c87bb4a606d4c981254
Suveybe Sena Kucuk <suveybesena@gmail.com>
2022-05-10 00:45:51 +0300

    remove unused line

--- app/src/main/java/com/suveybesena/shoppingapp/presentation/mainfeed/lipstick/LipstickFragment.kt
+++ app/src/main/java/com/suveybesena/shoppingapp/presentation/mainfeed/lipstick/LipstickFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -91,4 +90,5 @@ class LipstickFragment : Fragment() {
     private fun goToProductWebsite(bundle: Bundle) {
         findNavController().navigate(R.id.action_mainFeedFragment_to_webViewFragment, bundle)
     }
+
 }
